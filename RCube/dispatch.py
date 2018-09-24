7acbad0446ef69c6223a767cd05cba041b28f4d5
Turner <Turner@192.168.0.3>
2018-09-24 17:20:30 -0500

    Green Light |

--- RCube/dispatch.py
+++ RCube/dispatch.py
@@ -12,7 +12,7 @@ def dispatch(parm={}):
 
 def createCube(parm):
     if 'f' in parm :
-        cube = ['purple']*9 + ['yellow']*9 + ['blue']*9 +  ['white']*9 + ['red']*9 + ['orange']*9
+        cube = [parm['f']]*9 + ['yellow']*9 + ['blue']*9 +  ['white']*9 + ['red']*9 + ['orange']*9
     else:    
         cube = ['green']*9 + ['yellow']*9 + ['blue']*9 +  ['white']*9 + ['red']*9 + ['orange']*9
     return cube

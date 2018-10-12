cfeb41acb3d04d3a8904ec6fd7ced3468fdff35b
Turner <Turner@turners-mbp>
2018-10-12 11:52:17 -0500

    Red Light |

--- RCube/test/dispatchTest.py
+++ RCube/test/dispatchTest.py
@@ -349,7 +349,20 @@ class DispatchTest(unittest.TestCase):
         self.assertIn('status', resultDict)
         self.assertEquals('error:',resultDict['status'][0:6])
         
-    def test940_010_ShouldReturnErrorOnBadCubeCenters(self):
+    def test940_010_ShouldReturnErrorOnWrongCubeColorsSpecified(self):
+        queryString='op=check&cube=' + \
+                                  'f,f,f,f,f,f,f,f,f,' + \
+                                  'r,r,r,r,r,r,r,r,r,' + \
+                                  'b,b,b,b,b,b,b,b,b,' + \
+                                  'l,l,l,l,l,l,l,l,l,' + \
+                                  't,t,t,t,t,t,t,t,t,' + \
+                                  'u,u,u,u,u,u,u,u,u'
+        resultString = self.httpGetAndResponse(queryString)
+        resultDict = self.string2dict(resultString)
+        self.assertIn('status', resultDict)
+        self.assertEquals('error:',resultDict['status'][0:6])
+        
+    def test950_010_ShouldReturnErrorOnBadCubeCenters(self):
         queryString='op=check&f=f&r=r&b=b&l=l&t=t&u=u&cube=' + \
                                   'f,f,f,f,r,f,f,f,f,' + \
                                   'r,r,r,r,f,r,r,r,r,' + \

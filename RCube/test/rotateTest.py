b3de9f59d7e95f50b50e773ced8c6c63360deb4b
Turner <Turner@192.168.0.3>
2018-10-30 09:09:17 -0500

    Red Light |

--- RCube/test/rotateTest.py
+++ RCube/test/rotateTest.py
@@ -46,39 +46,56 @@ class RotateCubeTest(unittest.TestCase):
 #
 # Happy Path
 
-    def test100_010ShouldRotateFaceTopClockwise(self):
+#     def test100_610ShouldRotateFrontFaceOnlyClockwise(self):
+#         
+#         cube = ['0','1','2','3','f','5','6','7','8',
+#                 'r','r','r','r','r','r','r','r','r',
+#                 'b','b','b','b','b','b','b','b','b',
+#                 'l','l','l','l','l','l','l','l','l',
+#                 't','t','t','t','t','t','t','t','t',
+#                 'u','u','u','u','u','u','u','u','u',]
+#         face = 'f'
+#         resultCube = RCube.rotateCube(cube, face)
+#         expectedCube = ['6','3','0','7','f','1','8','5','2',
+#                 'r','r','r','r','r','r','r','r','r',
+#                 'b','b','b','b','b','b','b','b','b',
+#                 'l','l','l','l','l','l','l','l','l',
+#                 't','t','t','t','t','t','t','t','t',
+#                 'u','u','u','u','u','u','u','u','u',]
+#         self.assertEquals(expectedCube, resultCube)
+# 
+#     def test100_620ShouldRotateFrontFaceOnlyAntiClockwise(self):
+#         
+#         cube = ['0','1','2','3','f','5','6','7','8',
+#                 'r','r','r','r','r','r','r','r','r',
+#                 'b','b','b','b','b','b','b','b','b',
+#                 'l','l','l','l','l','l','l','l','l',
+#                 't','t','t','t','t','t','t','t','t',
+#                 'u','u','u','u','u','u','u','u','u',]
+#         face = 'F'
+#         resultCube = RCube.rotateCube(cube, face)
+#         expectedCube = ['2','5','8','1','f','7','0','3','6',
+#                 'r','r','r','r','r','r','r','r','r',
+#                 'b','b','b','b','b','b','b','b','b',
+#                 'l','l','l','l','l','l','l','l','l',
+#                 't','t','t','t','t','t','t','t','t',
+#                 'u','u','u','u','u','u','u','u','u',]
+#         self.assertEquals(expectedCube, resultCube)
+
+    def test100_630ShouldRotateFrontFaceAndTopEdgeClockwise(self):
         
         cube = ['0','1','2','3','f','5','6','7','8',
-                'r','r','r','r','r','r','r','r','r',
-                'b','b','b','b','b','b','b','b','b',
-                'l','l','l','l','l','l','l','l','l',
-                't','t','t','t','t','t','t','t','t',
-                'u','u','u','u','u','u','u','u','u',]
+                '9','10','11','12','r','14','15','16','17',
+                '18','19','20','21','b','23','24','25','26',
+                '27','28','29','30','l', '32','33','34','35',
+                '36','37','38','39','t','41','42','43','44',
+                '45','46','47','48','u','50','51','52','53',]
         face = 'f'
         resultCube = RCube.rotateCube(cube, face)
         expectedCube = ['6','3','0','7','f','1','8','5','2',
-                'r','r','r','r','r','r','r','r','r',
-                'b','b','b','b','b','b','b','b','b',
-                'l','l','l','l','l','l','l','l','l',
-                't','t','t','t','t','t','t','t','t',
-                'u','u','u','u','u','u','u','u','u',]
+                '42','10','11','43','r','14','44','16','17',
+                '18','19','20','21','b','23','24','25','26',
+                '27','28','29','30','l', '32','33','34','35',
+                '36','37','38','39','t','41','42','43','44',
+                '45','46','47','48','u','50','51','52','53',]
         self.assertEquals(expectedCube, resultCube)
-
-    def test100_020ShouldRotateFaceTopAntiClockwise(self):
-        
-        cube = ['0','1','2','3','f','5','6','7','8',
-                'r','r','r','r','r','r','r','r','r',
-                'b','b','b','b','b','b','b','b','b',
-                'l','l','l','l','l','l','l','l','l',
-                't','t','t','t','t','t','t','t','t',
-                'u','u','u','u','u','u','u','u','u',]
-        face = 'F'
-        resultCube = RCube.rotateCube(cube, face)
-        expectedCube = ['2','5','8','1','f','7','0','3','6',
-                'r','r','r','r','r','r','r','r','r',
-                'b','b','b','b','b','b','b','b','b',
-                'l','l','l','l','l','l','l','l','l',
-                't','t','t','t','t','t','t','t','t',
-                'u','u','u','u','u','u','u','u','u',]
-        self.assertEquals(expectedCube, resultCube)
-        
